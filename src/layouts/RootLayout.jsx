034374aebeec6d4e631b908db1788efa58f1daf1
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2026-03-01 05:20:19 +0600

    add navber,footer,google font

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../pages/shared/Navbar/Navbar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const RootLayout = () => {
     return (
-        <Outlet></Outlet>
+        <>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </>
     );
 };
 
